101b5ff0b9cc303bd68db0a7daa62ee4c5b90b01
Moremaster123 <juancarlin01@hotmail.com>
2025-12-27 10:54:31 -0600

    Se agrega ejercicios de arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+console.log ("===Array (aarreglos/matrices)===")
+
+
+let frutas = ["manzana","banana","cereza"]; //Array de frutas
+console.log("Frutas disponibles: ", frutas); //Forma de concatenar cosas es con coma , en vez de mas +
+console.log("Tearcera Fruta:", frutas[2]); //Acceso al tercer elemento, tambien el 0 es pocision uno en al vida real
+
+
+let numeros = [10,20,30,40,50]; //Array de numeros
+console.log("Numeros disponibles:", numeros);
+console.log("Segundo Numero", numeros[1]); //Acceso al segundo elemento, tambien el 0 es pocision uno en al vida real
